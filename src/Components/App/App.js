08a146c8aae5477aec37ce99d944b0ff1420fe6a
Gridnbird <mr.artcraft@gmail.com>
2021-01-26 17:00:29 +0200

    Performed the method operations to create a Method that Saves the Playlist to a User's Accound

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -44,6 +45,12 @@ class App extends React.Component {
   updatePlaylistName(name) {
     this.setState({ playlistName:  name });
   }
+
+  savePlaylist() {
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+
+  }
+
   render() {
     return (
       <div>
@@ -56,7 +63,8 @@ class App extends React.Component {
       <Playlist playlistName={this.state.playlistName} 
                 playlistTracks={this.state.playlistTracks}
                 onRemove={this.removeTrack}
-                onNameChange={this.updatePlaylistName} />
+                onNameChange={this.updatePlaylistName}
+                onSave={this.savePlaylist} />
     </div>
   </div>
 </div>
