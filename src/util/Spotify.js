e868d76a5398f48a0c07d5f09549d6f8d7a0f932
Gridnbird <mr.artcraft@gmail.com>
2021-01-26 22:09:36 +0200

    Obtain a Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,32 @@
+const clientId = '70c9a7206afd4e538c23133bb96c893b';
+const redirectUri = 'http://localhost:3000/';
+let accessToken;
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        } 
+
+        // Check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clears the parameters, allowing us to grab a new access token when it expires. 
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Acess Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?
+            client_id=${clientId}&response_type=token&scope=playlist-modify-public
+            &redirect_uri=${redirectUri}`;
+            window.location = accessUri;
+        }
+
+    }
+
+}
+
+export default Spotify;
