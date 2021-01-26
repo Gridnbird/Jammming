f04bc6270027f754206ac7f63049ba94978e9004
Gridnbird <mr.artcraft@gmail.com>
2021-01-26 16:26:39 +0200

    Creating method operations to remove Tracks from Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -32,6 +33,12 @@ class App extends React.Component {
     }
     tracks.push(track);
     this.setState({ playlistTracks: tracks });
+  }
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState({ playlistTracks: tracks });
+
   }
   render() {
     return (
@@ -43,7 +50,8 @@ class App extends React.Component {
       <SearchResults searchResults={this.state.searchResults}
                      onAdd={this.addTrack} />
       <Playlist playlistName={this.state.playlistName} 
-                playlistTracks={this.state.playlistTracks} />
+                playlistTracks={this.state.playlistTracks}
+                onRemove={this.removeTrack} />
     </div>
   </div>
 </div>
